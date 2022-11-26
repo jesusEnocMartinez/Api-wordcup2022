a65862abedece4f195bda91b3ff575809316d459
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2022-11-25 23:02:04 -0600

    se agrego clase abtrata
    sse agrego clases abstratas

--- src/main/java/com/jesusenoc/apimundial/Service/AbstracCliente.java
+++ src/main/java/com/jesusenoc/apimundial/Service/AbstracCliente.java
@@ -1,4 +1,24 @@
 package com.jesusenoc.apimundial.Service;
 
-public class AbstracCliente {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+public abstract class AbstracCliente {
+
+    @Value("${worldcup.base-url}")
+    protected String baseUrl;
+
+    protected final RestTemplate restTemplate;
+
+    protected AbstracCliente(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    protected HttpHeaders buildAuthToken(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", "Bearer " + token);
+        return headers;
+    }
 }
--- src/main/java/com/jesusenoc/apimundial/Service/UserServiceIml.java
+++ src/main/java/com/jesusenoc/apimundial/Service/UserServiceIml.java
@@ -1,4 +1,45 @@
 package com.jesusenoc.apimundial.Service;
 
-public class UserServiceIml {
+import com.jesusenoc.apimundial.Login.LoginRequest;
+import com.jesusenoc.apimundial.Login.LoginResponsible;
+import com.jesusenoc.apimundial.Login.RegisterUserRequest;
+import com.jesusenoc.apimundial.Login.RegisterUserResponsible;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class UserServiceIml  extends AbstracCliente implements UserService{
+    public UserServiceIml(RestTemplate restTemplate) {
+        super(restTemplate);
+    }
+    @Override
+    public RegisterUserResponsible create(RegisterUserRequest user) {
+        String uri = baseUrl + "/user";
+        log.info("Going to create {}", user);
+        ResponseEntity<RegisterUserResponsible> response = restTemplate.postForEntity(uri, user, RegisterUserResponsible.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            log.info("Successfully user creation: {}", response.getBody().getStatus());
+            return response.getBody();
+        }
+        log.error("Error in user creation - httpStatus was: {}", response.getStatusCode());
+        throw new RuntimeException("Error");
+    }
+
+    @Override
+    public LoginResponsible login(LoginRequest loginRequest) {
+
+        String uri = baseUrl + "/user/login";
+        log.info("Going to login {}", loginRequest);
+        ResponseEntity<LoginResponsible> response = restTemplate.postForEntity(uri, loginRequest, LoginResponsible.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            log.info("Successfully user creation: {}", response.getBody().getStatus());
+            return response.getBody();
+        }
+        log.error("Error in user login - httpStatus was: {}", response.getStatusCode());
+        throw new RuntimeException("Error");
+    }
 }
